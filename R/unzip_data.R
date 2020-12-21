4a7b7c2e4584ff94b153a46a9c7cc31a29bce610
Joel Nitta <joelnitta@gmail.com>
2020-12-21 03:22:47 +0000

    Move unzipping data to a separate script

---
+++ R/unzip_data.R
@@ -0,0 +1,21 @@
+source("R/packages.R")
+source("R/functions.R")
+
+# Unzip data from Nitta et al. 2017 Ecol. Monographs from Dryad
+# The dataset must be downloaded first by going to
+# https://datadryad.org/stash/dataset/doi:10.5061/dryad.df59g, clicking on
+# "Download dataset", and saving to the "data" folder in this project.
+unzip_nitta_2017(
+  zipped_path = file_in("data/doi_10.5061_dryad.df59g__v1.zip"),
+  unzip_path = "data/nitta_2017")
+
+# Unzip data from Nitta et al. 2020 New Phyt. from Dryad
+# The dataset must be downloaded first by going to
+# https://datadryad.org/stash/dataset/doi:10.5061/dryad.fqz612jps, clicking on
+# "Download dataset", and saving to the "data" folder in this project.
+unzip(
+  zipfile = file_in("data/doi_10.5061_dryad.fqz612jps__v4.zip"), 
+  files = "moorea_climate.csv", 
+  exdir = file_out("data/nitta_2020/"), 
+  junkpaths = TRUE, 
+  overwrite = TRUE)
