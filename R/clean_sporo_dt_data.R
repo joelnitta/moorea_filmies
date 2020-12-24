2b14e545aac2384ee15decc2e4c5a21a388ce904
Joel Nitta <joelnitta@gmail.com>
2020-12-24 02:16:43 +0000

    WIP: add time stamps to filmy DT data by joining with PAM data

--- R/clean_sporo_dt_data.R
+++ R/clean_sporo_dt_data.R
@@ -140,10 +140,15 @@ filmy_dt_2013_1 <- filmy_dt_2013_1_raw %>%
     dry_time = parse_number(duration_of_treatment),
     individual,
     yield_pre = wet_yield,
+    memory_pre = wet_last_memory,
     yield_30min = x30min_yield,
+    memory_30min = x30min_last_memory,
     yield_24hr = x24hr_mini_pam_data,
+    memory_24hr = x24_hr_last_memory,
     yield_48hr = x48hr_mini_pam_data,
-    yield_72hr = x72hr_mini_pam_c_apii_only) %>% 
+    memory_48hr = x48_hr_last_memory,
+    yield_72hr = x72hr_mini_pam_c_apii_only,
+    memory_72hr = x72_hr_last_memory) %>% 
   # Fix species names
   mutate(
     species = case_when(
@@ -163,8 +168,10 @@ filmy_dt_2013_1 <- filmy_dt_2013_1_raw %>%
     ),
     dry_time = as.factor(dry_time)
   ) %>%
-  mutate(yield_pre = ifelse(salt != "Control", 1000*yield_pre, yield_pre)) %>%
-  mutate(dataset = "2013_1") %>%
+  mutate(
+    yield_pre = ifelse(salt != "Control", 1000*yield_pre, yield_pre),
+    dataset = "2013_1",
+    across(contains("memory") & where(is.character), parse_number)) %>%
   check_dt_data
 
 filmy_dt_2013_2_raw <- read_excel("data_raw/2013/Filmy Fern DT 2 Pbor.xlsx", sheet = 1)
@@ -178,9 +185,13 @@ filmy_dt_2013_2 <- filmy_dt_2013_2_raw %>%
     dry_time = duration_of_treatment,
     individual,
     yield_pre = yield_wet,
+    memory_pre = memory_wet,
     yield_30min = yield_20_min_recover,
+    memory_30min = memory_20_min_recover,
     yield_24hr = yield_24hr_recover,
+    memory_24hr = memory_24hr_recover,
     yield_48hr = yield_48hr_recover,
+    memory_48hr = memory_48hr_recover,
     weight_pre = wet_weight_g,
     weight_desiccated_2d = desiccated_weight_2d_g,
     weight_desiccated_15d = desiccated_weight_15d,
@@ -202,7 +213,9 @@ filmy_dt_2013_2 <- filmy_dt_2013_2_raw %>%
     )
   ) %>% 
   select(-c(weight_desiccated_2d, weight_desiccated_15d)) %>%
-  mutate(dataset = "2013_2") %>%
+  mutate(
+    dataset = "2013_2",
+    across(contains("memory") & where(is.character), parse_number)) %>%
   check_dt_data
 
 filmy_dt_2013_3_raw <- read_excel("data_raw/2013/Filmy Fern DT 3 Hmul, Hdig.xlsx", sheet = 1)
@@ -216,9 +229,13 @@ filmy_dt_2013_3 <- filmy_dt_2013_3_raw %>%
     dry_time = parse_number(duration_of_treatment),
     individual,
     yield_pre = mini_pam_data_wet,
+    memory_pre = last_memory_for_wet_data,
     yield_30min = mini_pam_data_30min,
+    memory_30min = last_memory_for_30_min_data,
     yield_24hr = mini_pam_data_24hr,
+    memory_24hr = last_memory_for_24hr,
     yield_48hr = parse_number(mini_pam_data_48hr),
+    memory_48hr = last_memory_for_48hr,
     weight_pre = wet_weight_g,
     weight_desiccated_2d = x2d_desiccated_weight_g,
     weight_desiccated_15d = dessicated_weight,
@@ -241,7 +258,9 @@ filmy_dt_2013_3 <- filmy_dt_2013_3_raw %>%
     )
   ) %>% 
   select(-c(weight_desiccated_2d, weight_desiccated_15d)) %>%
-  mutate(dataset = "2013_3") %>%
+  mutate(
+    dataset = "2013_3",
+    across(contains("memory") & where(is.character), parse_number)) %>%
   check_dt_data
 
 filmy_dt_2013_4_raw <- read_excel("data_raw/2013/Filmy Fern DT 4 Hpol.xlsx", sheet = 1)
@@ -255,9 +274,13 @@ filmy_dt_2013_4 <- filmy_dt_2013_4_raw %>%
     dry_time = parse_number(duration_of_treatment),
     individual,
     yield_pre = mini_pam_data_wet,
+    memory_pre = last_memory_for_wet_data,
     yield_30min = yield_20_min_recover,
+    memory_30min = memory_20_min,
     yield_24hr = parse_number(yield_24hr_recover),
+    memory_24hr = memory_24hr_recover,
     yield_48hr = yield_48hr_recovery,
+    memory_48hr = memory_48hr_recovery,
     weight_pre = wet_weight_g,
     weight_desiccated_2d = parse_number(desiccated_weight_g),
     weight_desiccated_15d = desiccated_weight_15d,
@@ -280,13 +303,15 @@ filmy_dt_2013_4 <- filmy_dt_2013_4_raw %>%
     )
   ) %>% 
   select(-c(weight_desiccated_2d, weight_desiccated_15d)) %>%
-  mutate(dataset = "2013_4") %>%
+  mutate(
+    dataset = "2013_4",
+    across(contains("memory") & where(is.character), parse_number)) %>%
   check_dt_data
 
 filmy_dt_2013_5_raw <- read_excel("data_raw/2013/Filmy Fern DT Set 5 DONE.xlsx", sheet = 1)
 
 filmy_dt_2013_5 <- filmy_dt_2013_5_raw %>%
-  clean_names() %>% 
+  clean_names() %>%
   transmute(
     species,
     salt = salt_treatment, 
@@ -294,10 +319,14 @@ filmy_dt_2013_5 <- filmy_dt_2013_5_raw %>%
     dry_time = replace_na(duration_of_treatment, 2),
     individual,
     yield_pre = mini_pam_data_wet,
+    memory_pre = last_memory_for_wet_data_7_26,
     # fvfm_dry = yield_dry,
     yield_30min = yield_20_min_recover_7_28_18_50pm,
+    memory_30min = memory_20_min,
     yield_24hr = yield_24hr_recover_7_29,
+    memory_24hr = memory_24hr_recover,
     yield_48hr = yield_48hr_recovery_7_30,
+    memory_48hr = memory_48hr_recovery,
     weight_pre = wet_weight_g,
     weight_desiccated = desiccated_weight_2d_g,
     weight_30min = mass_20min_recover,
@@ -313,8 +342,11 @@ filmy_dt_2013_5 <- filmy_dt_2013_5_raw %>%
       TRUE ~ NA_character_
     )) %>% 
   # Convert dry time to factor (control will be NA)
-  mutate(dry_time = as.factor(dry_time)) %>%
-  mutate(dataset = "2013_5") %>%
+  mutate(
+    dry_time = as.factor(dry_time),
+    dataset = "2013_5",
+    across(contains("memory") & where(is.character), parse_number)
+  ) %>%
   check_dt_data
 
 # 2014 data ----
@@ -330,10 +362,14 @@ filmy_dt_2014 <- filmy_dt_2014_raw %>%
     dry_time = time,
     individual,
     yield_pre = wet_fv_fm,
+    memory_pre = wet_mem,
     # fvfm_dry = dry_fv_fm,
     yield_30min = x30min_fv_fm,
+    memory_30min = x30min_mem,
     yield_24hr = x24hr_fv_fm,
+    memory_24hr = x24hr_mem,
     yield_48hr = x48hr_fv_fm,
+    memory_48hr = x48hr_mem,
     weight_pre = wet_weight,
     weight_desiccated = dry_weight_7_2_14,
     weight_30min = x30min_weight_7_2_14,
@@ -351,8 +387,9 @@ filmy_dt_2014 <- filmy_dt_2014_raw %>%
       TRUE ~ NA_character_
     )) %>% 
   # Convert dry time to factor (control will be NA)
-  mutate(dry_time = dry_time %>% parse_number %>% as.factor) %>%
-  mutate(dataset = "2014") %>%
+  mutate(dry_time = dry_time %>% parse_number %>% as.factor,
+         dataset = "2014",
+         across(contains("memory") & where(is.character), parse_number)) %>%
   check_dt_data
 
 # Combine data from different years ----
@@ -365,10 +402,27 @@ filmy_sporo_dt <- bind_rows(
   filmy_dt_2013_5, 
   filmy_dt_2014) %>%
   select(
-    species, salt, dry_time, individual, dataset, contains("weight"), contains("yield")
+    species, salt, dry_time, individual, dataset, contains("weight"), contains("yield"), contains("memory")
   ) %>%
   mutate(generation = "sporophyte") %>%
   check_dt_data
+
+# Join with PAM data to add timestamps ----
+# Convert to long format
+filmy_sporo_dt %>%
+  select(-contains("yield")) %>%
+  pivot_longer(names_to = "condition", values_to = "memory", contains("memory")) %>%
+  filter(!is.na(memory)) %>%
+  mutate(condition = str_remove_all(condition, "memory_") %>% 
+           factor(levels = c("pre", "30min", "24hr", "48hr", "72hr"))
+  ) %>%
+  arrange(dry_time, condition, salt, species, individual) %>%
+  mutate(pam_set = case_when(
+    dry_time == 15 & condition %in% c("30min", "24hr", "48hr", "72hr") ~ "set2",
+    TRUE ~ "set1"
+  )) %>%
+  assert_rows(col_concat, is_uniq, pam_set, memory)
+
 
 # Write to data_raw/ ----
 write_csv(filmy_sporo_dt, "data/filmy_sporo_dt.csv")
