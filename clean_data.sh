6572a5d7d0811247a861a0db6734144131e4d49c
Joel Nitta <joelnitta@gmail.com>
2021-03-13 07:25:38 +0000

    Change order of data cleaning scripts

--- clean_data.sh
+++ clean_data.sh
@@ -2,7 +2,10 @@
 
 # Run each data cleaning script in its own R session
 
+# Data files "filmy_growth_habit.csv" and those extracted by R/unzip_data.R must
+# already be present in data/
+
+Rscript R/clean_specimen_data.R
 Rscript R/clean_chamber_data.R 
 Rscript R/clean_dt_data.R
 Rscript R/clean_lc_data.R
-Rscript R/clean_specimen_data.R
